0675d0555a7d2ce6b083f5c5c4326b9e4cf1ab09
ykatabuc <skay5677@yahoo.co.jp>
2024-02-18 22:53:05 +0900

    Move key

--- backend/app/todo-api/settings.py
+++ backend/app/todo-api/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3ggo-c+sgavjuh#sqget+wt%%e)@5n=3b01d120np#c)+))iq2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
