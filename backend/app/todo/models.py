87b432ea4bc6e82b1b8d824b82665843d94c73b5
ykatabuc <skay5677@yahoo.co.jp>
2024-02-23 07:08:40 +0900

    Mod ToDo Model

--- backend/app/todo/models.py
+++ backend/app/todo/models.py
@@ -2,4 +2,27 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-    text: models.CharField(max_length=200)
+    class Status(models.IntegerChoices):
+        READY = 1, 'Ready'
+        DOING = 2, 'Doing'
+        DONE = 3, 'Done'
+    text = models.TextField(
+        max_length=200,
+        help_text='やること',
+        default=''
+    )
+    state = models.IntegerField(
+        choices=Status.choices,
+        default=Status.READY
+    )
+    due_at = models.DateField(
+        help_text = 'いつやるのか'
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        help_text='作成日'
+    )
+    updated_at = models.DateField(
+        auto_now=True,
+        help_text='更新日'
+    )
