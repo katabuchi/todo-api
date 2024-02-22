c08b9db19bb405a8815dab0b22b69ba5c0826382
ykatabuc <skay5677@yahoo.co.jp>
2024-02-23 08:00:34 +0900

    Change Database

--- backend/app/todo-api/settings.py
+++ backend/app/todo-api/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'todo-api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'HOST': 'db',
+        'PORT': '3306',
+        'USER': 'user',
+        'PASSWORD': 'sample'
     }
 }
 
