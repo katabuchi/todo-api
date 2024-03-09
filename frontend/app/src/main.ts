571ad628ac61b631a7111bae6ee3d01320b77b84
ykatabuc <skay5677@yahoo.co.jp>
2024-03-09 13:44:37 +0900

    feat: routing to ToDoList

--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ToDoList from './views/ToDoList.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: "/", component:  ToDoList}
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
--- frontend/app/src/views/ToDoList.vue
+++ frontend/app/src/views/ToDoList.vue
@@ -1,8 +1,24 @@
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { useToDoList } from '../components/ToDoList';
+
+const { fetchToDoList, todoList } = useToDoList();
+
+const sample = ref('');
+
+onMounted(async() => {
+  await fetchToDoList().catch((e) => {
+    console.error(`[ERROR] ${e}`);
+  });
+  if (todoList.length !== 0) {
+    sample.value =  todoList[0].text;
+  }
+});
 
 </script>
 
 <template>
   <div>
+    <p>{{ sample }}</p>
   </div>
 </template>
