f178c45e5d7a66241a8c19577cfa19b8eede9a69
ykatabuc <skay5677@yahoo.co.jp>
2024-03-09 13:45:25 +0900

    fix: useToDoList

--- frontend/app/src/components/ToDoList.ts
+++ frontend/app/src/components/ToDoList.ts
@@ -1,11 +1,23 @@
-import { ref } from 'vue'
+import { reactive } from 'vue'
+
+// APIからのレスポンス(Repository層に入れるようなデータクラス)
+type ToDoResponse = {
+    text: string
+}
+
+// 公開するToDoドメインモデル(Domain層に入れるようなデータクラス)
+type ToDo = {
+    text: string
+}
 
 export function useToDoList() {
-    const todoList = ref([])
+    const todoList = reactive<ToDo[]>([])
     const fetchToDoList = async () => {
         const response = await fetch("http://localhost:8000/todos/todo/")
-        const data = await response.json()
-        todoList.value = data
+        const data: Array<ToDoResponse> = await response.json()
+        data.forEach(element => {
+            todoList.push({text: element.text})
+        });
         return data
     }
     return {
